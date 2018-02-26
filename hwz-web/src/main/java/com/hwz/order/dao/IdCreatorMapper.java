0f4eb88ffcbe78af3bcd54cc82224296aa65b432
zhangzaipeng <282354184@qq.com>
2018-02-26 20:58:12 +0800

    清理目录

--- hwz-web/src/main/java/com/hwz/order/dao/IdCreatorMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hwz.order.dao;
-import com.hwz.order.domain.IdCreator;
-import com.hwz.platform.mybatis.DefaultMapper;
-
-public interface IdCreatorMapper extends DefaultMapper {
-    void nextValue(IdCreator idCreator);
-
-    IdCreator curValue(String idName);
-}
--- hwz-web/src/test/java/order/GetOrderId.java
+++ hwz-web/src/test/java/order/GetOrderId.java
@@ -7,31 +7,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.hwz.order.service.OrderService;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:spring/spring-*.xml"})
 public class GetOrderId extends AbstractJUnit4SpringContextTests{
 	
-    @Autowired
-    private OrderService orderService;
-    
 	@Test
     public void testHello() throws InterruptedException{
 		
-		long start = System.currentTimeMillis();
-    	for (int i = 0; i < 10; i++) {
-    		OrderRunnable orderRunnable = new OrderRunnable(i+"",orderService);  
-            Thread thread=new Thread(orderRunnable);
-            
-            thread.start();
-            
-            thread.sleep(10l);
-            
-		}
-    	
-    	System.out.println("end=" + (System.currentTimeMillis() - start));
-    	
-		
+    	System.out.println("end hello order");
+
     }
 }
